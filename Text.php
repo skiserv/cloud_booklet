41d980aad39abcedbf67c0aaf774b5d227144ef4
SkiServ <skirata@skiserv.eu>
2025-07-01 18:02:35 +0200

    feat: extract tags from markdown

--- Text.php
+++ Text.php
@@ -6,6 +6,7 @@ class Text
     public function __construct(
         public string $title,
         public string $content,
+        public array $tags = [],
     ) {
         $this->id = str_replace(
             [" ", "'", "À", "à"],
@@ -22,8 +23,9 @@ class Text
 
 class TextFactory
 {
-    public static function createFromMarkdownFile(string $file)
+    public static function createFromMarkdownFile(string $file): Text
     {
+        /* Get first high level title as main title, or by default first words */
         $title = null;
         while ($line = strtok($file, "\n\r")) {
             if (str_starts_with($line, "# ")) {
@@ -35,9 +37,22 @@ class TextFactory
             $title = substr($file, 0, 10);
         }
 
+        /* Extracts tags if exists and remove it from content */
+        $tags = [];
+        $tagsRegex = "/(tags:).*(\n|\Z)/";
+        if (preg_match(
+            $tagsRegex,
+            $file,
+            $tagsAsString,
+        )) {
+            $tags = explode(" ", substr($tagsAsString[0], 6));
+            $file = preg_replace($tagsRegex, "", $file);
+        }
+
         return (new Text(
             title: $title,
-            content: $file
+            content: $file,
+            tags: $tags,
         ));
     }
 }
